0aed7032e9ed7c199530fab0c03d3cc0e04cc5b3
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2020-08-17 10:02:22 -0300

    preparing to deploy test

--- playout/settings.py
+++ playout/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&_9o8nrcfos(=wz-yii7dr_ixi41_=75r@qxm!8he$5mco(=+7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -126,12 +126,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #Bucket confs
-AWS_ACCESS_KEY_ID = 'AKIAYEFGZ2RHCMD75XFN'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'bruno.video'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
